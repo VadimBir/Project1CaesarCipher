71ea345fd4019106ba984689c209c9270cbafcb4
VadimBir <vasumrus@gmail.com>
2023-12-08 13:56:08 -0600

    Ceasar Cypher Base Code

---
+++ src/CaesarCipherDriver.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class CaesarCipherDriver {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Step 1: Get plaintext input
+
+        // Step 2: Get seed for random number generation
+
+        // Step 3: Generate the key
+
+        // Step 4: Encode the plaintext
+
+        // Step 5: Display the ciphertext
+
+    }
+}
