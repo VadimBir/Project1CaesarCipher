de7af1ebe93f4658d6c750cde6d5a142ab345373
Vadimbir <vasumrus@gmail.com>
2023-12-27 16:17:28 -0600

    Base code and Unittests

--- src/CaesarCipherDriver.java
+++ src/CaesarCipherDriver.java
@@ -3,17 +3,23 @@ import java.util.Scanner;
 
 public class CaesarCipherDriver {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // Only once
 
         // Step 1: Get plaintext input
+        System.out.print("Enter a string of exactly twelve characters you want to encrypt:\n");
+        // Code here
 
         // Step 2: Get seed for random number generation
 
         // Step 3: Generate the key
+        Random random = new Random(seed); // Only once and create seed variable
+        // Code here
 
         // Step 4: Encode the plaintext
+        // Encryption logic here
 
         // Step 5: Display the ciphertext
-
+        // Exactly as required
     }
+
 }
---
+++ src/TestOutput.java
@@ -0,0 +1,81 @@
+import jdk.jfr.Timespan;
+import org.junit.jupiter.api.*;
+
+import java.io.*;
+
+import java.io.*;
+import java.nio.file.*;
+
+
+public class TestOutput {
+
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    // test program running for 0.2 seconds
+
+    @Test
+    public void testRunExecution() {
+        // Set the inputs (12-character string and a fixed seed value)
+        String input = "HELLOWORLD!!\n7\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        // Call the main method
+        CaesarCipherDriver.main(null);
+        // Extract the output
+        String output = outputStreamCaptor.toString();
+        System.out.println(output);
+        // Check if the output contains the expected ciphertext
+        Assertions.assertTrue(true, "Output: " + output);
+    }
+    @Test
+    public void testBaseCiphertextOutput() {
+        // Set the inputs (12-character string and a fixed seed value)
+        String input = "HELLOWORLD!!\n7\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        // Call the main method
+        CaesarCipherDriver.main(null);
+
+        // Extract the output
+        String output = outputStreamCaptor.toString();
+
+        // Check if the output contains the expected ciphertext
+        Assertions.assertTrue(output.contains("Ciphertext: "));
+        String[] parts = output.split("Ciphertext: ");
+        if (parts.length > 1) {
+            String cipherText = parts[1].split("\n")[0].trim();
+            Assertions.assertEquals("XQOVWWGEXZKO", cipherText);
+        } else {
+            Assertions.fail("Ciphertext not found in output");
+        }
+    }
+    @Test
+    public void testFailCiphertextOutput() {
+        // Set the inputs (12-character string and a fixed seed value)
+        String input = "HELLO";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        // Call the main method
+        CaesarCipherDriver.main(null);
+        // Extract the output
+        String output = outputStreamCaptor.toString();
+        String expected = "Please read directions and try again!";
+        // Check if the output contains the expected ciphertext
+        Assertions.assertTrue(output.contains(expected), "Expected: " + expected + "\nActual: " + output);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(System.out);
+        System.setIn(System.in);
+    }
+}
+
+
